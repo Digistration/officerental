70dab420bed896e754c23985f92071471cb36cd0
James Devine <digistration@gmail.com>
2018-12-02 12:03:47 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,263 @@
+
+window.sr = ScrollReveal();
+
+// HERO
+sr.reveal('.hero', {
+  duration: 6000,
+  opacity: 0
+})
+
+
+// SECTION 1
+// INFO
+sr.reveal('.sec1h1', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.sec1p', {
+  duration: 1500,
+  // delay: 500,
+  distance: '10%',
+  origin: 'bottom'
+})
+//IMAGE
+sr.reveal('.sec1_1st', {
+  duration: 1750,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.sec1_2nd', {
+  duration: 1750,
+  delay: 1250,
+  opacity: 0
+})
+sr.reveal('.sec1_3rd', {
+  duration: 1750,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec1_4th', {
+  duration: 1750,
+  delay: 1750,
+  opacity: 0
+})
+sr.reveal('.sec1_5th', {
+  duration: 1750,
+  delay: 2000,
+  opacity: 0
+})
+
+// SECTION 2
+// INFO
+sr.reveal('.sec2h1', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.sec2p', {
+  duration: 1500,
+  // delay: 500,
+  distance: '10%',
+  origin: 'bottom'
+})
+// IMAGE
+sr.reveal('.sec2_1st', {
+  duration: 1500,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.sec2_2nd', {
+  duration: 1500,
+  delay: 1250,
+  opacity: 0
+})
+sr.reveal('.sec2_3rd', {
+  duration: 1500,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec2_4th', {
+  duration: 1500,
+  delay: 1750,
+  opacity: 0
+})
+sr.reveal('.sec2_5th', {
+  duration: 1500,
+  delay: 2000,
+  opacity: 0
+})
+sr.reveal('.sec2_6th', {
+  duration: 1500,
+  delay: 2250,
+  opacity: 0
+})
+sr.reveal('.sec2_7th', {
+  duration: 1500,
+  delay: 2500,
+  opacity: 0
+})
+sr.reveal('.sec2_8th', {
+  duration: 1500,
+  delay: 2750,
+  opacity: 0
+})
+
+// SECTION 3
+// INFO
+sr.reveal('.sec3h1', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.sec3p', {
+  duration: 1500,
+  // delay: 500,
+  distance: '10%',
+  origin: 'bottom'
+})
+// IMAGE
+sr.reveal('.sec3_1st', {
+  duration: 1750,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.sec3_2nd', {
+  duration: 1750,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec3_3rd', {
+  duration: 1750,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec3_4th', {
+  duration: 1750,
+  delay: 2000,
+  opacity: 0
+})
+sr.reveal('.sec3_5th', {
+  duration: 1750,
+  delay: 2000,
+  opacity: 0
+})
+
+// SECTION 4
+// INFO
+sr.reveal('.sec4h1', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.sec4p', {
+  duration: 1500,
+  // delay: 500,
+  distance: '10%',
+  origin: 'bottom'
+})
+// IMAGE
+sr.reveal('.sec4_1st', {
+  duration: 1750,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.sec4_2nd', {
+  duration: 1750,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec4_3rd', {
+  duration: 1750,
+  delay: 2000,
+  opacity: 0
+})
+sr.reveal('.sec4_4th', {
+  duration: 1750,
+  delay: 2500,
+  opacity: 0
+})
+sr.reveal('.sec4_5th', {
+  duration: 1750,
+  delay: 3000,
+  opacity: 0
+})
+
+// SECTION 5
+// INFO
+sr.reveal('.sec5h1', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.sec5p', {
+  duration: 1500,
+  // delay: 500,
+  distance: '10%',
+  origin: 'bottom'
+})
+// IMAGE
+sr.reveal('.sec5_1st', {
+  duration: 1500,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.sec5_2nd', {
+  duration: 1500,
+  delay: 1250,
+  opacity: 0
+})
+sr.reveal('.sec5_3rd', {
+  duration: 1500,
+  delay: 1500,
+  opacity: 0
+})
+sr.reveal('.sec5_4th', {
+  duration: 1500,
+  delay: 1750,
+  opacity: 0
+})
+sr.reveal('.sec5_5th', {
+  duration: 1500,
+  delay: 2000,
+  opacity: 0
+})
+sr.reveal('.sec5_6th', {
+  duration: 1500,
+  delay: 2250,
+  opacity: 0
+})
+sr.reveal('.sec5_7th', {
+  duration: 1500,
+  delay: 2500,
+  opacity: 0
+})
+sr.reveal('.sec5_8th', {
+  duration: 1500,
+  delay: 2750,
+  opacity: 0
+})
+
+// SECTION 6
+// INFO
+sr.reveal('.quote', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+sr.reveal('.contact', {
+  duration: 1500,
+  distance: '50%',
+  origin: 'bottom'
+})
+// IMAGE
+sr.reveal('.map', {
+  duration: 4000,
+  delay: 1000,
+  opacity: 0
+})
+sr.reveal('.location', {
+  duration: 2000,
+  delay: 1250,
+  opacity: 0
+})
