2404c2e7881cf64828cf4384090fecba5bcaf289
James Devine <digistration@gmail.com>
2019-03-14 18:00:15 +0800

    Final

--- js/main.js
+++ js/main.js
@@ -11,260 +11,260 @@ sr.reveal('.hero', {
 // SECTION 1
 // INFO
 sr.reveal('.sec1h1', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.sec1p', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '10%',
   origin: 'bottom'
 })
 //IMAGE
 sr.reveal('.sec1_1st', {
-  duration: 1750,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.sec1_2nd', {
-  duration: 1750,
-  delay: 1250,
+  duration: 2000,
+  delay: 1100,
   opacity: 0
 })
 sr.reveal('.sec1_3rd', {
-  duration: 1750,
-  delay: 1500,
+  duration: 2000,
+  delay: 1300,
   opacity: 0
 })
 sr.reveal('.sec1_4th', {
-  duration: 1750,
-  delay: 1750,
+  duration: 2000,
+  delay: 1500,
   opacity: 0
 })
 sr.reveal('.sec1_5th', {
-  duration: 1750,
-  delay: 2000,
+  duration: 2000,
+  delay: 1700,
   opacity: 0
 })
 
 // SECTION 2
 // INFO
 sr.reveal('.sec2h1', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.sec2p', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '10%',
   origin: 'bottom'
 })
 // IMAGE
 sr.reveal('.sec2_1st', {
-  duration: 1500,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.sec2_2nd', {
-  duration: 1500,
-  delay: 1250,
+  duration: 2000,
+  delay: 900,
   opacity: 0
 })
 sr.reveal('.sec2_3rd', {
-  duration: 1500,
-  delay: 1500,
+  duration: 2000,
+  delay: 1000,
   opacity: 0
 })
 sr.reveal('.sec2_4th', {
-  duration: 1500,
-  delay: 1750,
+  duration: 2000,
+  delay: 1100,
   opacity: 0
 })
 sr.reveal('.sec2_5th', {
-  duration: 1500,
-  delay: 2000,
+  duration: 2000,
+  delay: 1200,
   opacity: 0
 })
 sr.reveal('.sec2_6th', {
-  duration: 1500,
-  delay: 2250,
+  duration: 2000,
+  delay: 1300,
   opacity: 0
 })
 sr.reveal('.sec2_7th', {
-  duration: 1500,
-  delay: 2500,
+  duration: 2000,
+  delay: 1400,
   opacity: 0
 })
 sr.reveal('.sec2_8th', {
-  duration: 1500,
-  delay: 2750,
+  duration: 2000,
+  delay: 1500,
   opacity: 0
 })
 
 // SECTION 3
 // INFO
 sr.reveal('.sec3h1', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.sec3p', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '10%',
   origin: 'bottom'
 })
 // IMAGE
 sr.reveal('.sec3_1st', {
-  duration: 1750,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.sec3_2nd', {
-  duration: 1750,
-  delay: 1500,
+  duration: 2000,
+  delay: 900,
   opacity: 0
 })
 sr.reveal('.sec3_3rd', {
-  duration: 1750,
-  delay: 1500,
+  duration: 2000,
+  delay: 1000,
   opacity: 0
 })
 sr.reveal('.sec3_4th', {
-  duration: 1750,
-  delay: 2000,
+  duration: 2000,
+  delay: 1100,
   opacity: 0
 })
 sr.reveal('.sec3_5th', {
-  duration: 1750,
-  delay: 2000,
+  duration: 2000,
+  delay: 1200,
   opacity: 0
 })
 
 // SECTION 4
 // INFO
 sr.reveal('.sec4h1', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.sec4p', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '10%',
   origin: 'bottom'
 })
 // IMAGE
 sr.reveal('.sec4_1st', {
-  duration: 1750,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.sec4_2nd', {
-  duration: 1750,
-  delay: 1500,
+  duration: 2000,
+  delay: 900,
   opacity: 0
 })
 sr.reveal('.sec4_3rd', {
-  duration: 1750,
-  delay: 2000,
+  duration: 2000,
+  delay: 1000,
   opacity: 0
 })
 sr.reveal('.sec4_4th', {
-  duration: 1750,
-  delay: 2500,
+  duration: 2000,
+  delay: 1100,
   opacity: 0
 })
 sr.reveal('.sec4_5th', {
-  duration: 1750,
-  delay: 3000,
+  duration: 2000,
+  delay: 1200,
   opacity: 0
 })
 
 // SECTION 5
 // INFO
 sr.reveal('.sec5h1', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.sec5p', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '10%',
   origin: 'bottom'
 })
 // IMAGE
 sr.reveal('.sec5_1st', {
-  duration: 1500,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.sec5_2nd', {
-  duration: 1500,
-  delay: 1250,
+  duration: 2000,
+  delay: 900,
   opacity: 0
 })
 sr.reveal('.sec5_3rd', {
-  duration: 1500,
-  delay: 1500,
+  duration: 2000,
+  delay: 1000,
   opacity: 0
 })
 sr.reveal('.sec5_4th', {
-  duration: 1500,
-  delay: 1750,
+  duration: 2000,
+  delay: 1000,
   opacity: 0
 })
 sr.reveal('.sec5_5th', {
-  duration: 1500,
-  delay: 2000,
+  duration: 2000,
+  delay: 1100,
   opacity: 0
 })
 sr.reveal('.sec5_6th', {
-  duration: 1500,
-  delay: 2250,
+  duration: 2000,
+  delay: 1200,
   opacity: 0
 })
 sr.reveal('.sec5_7th', {
-  duration: 1500,
-  delay: 2500,
+  duration: 2000,
+  delay: 1300,
   opacity: 0
 })
 sr.reveal('.sec5_8th', {
-  duration: 1500,
-  delay: 2750,
+  duration: 2000,
+  delay: 1400,
   opacity: 0
 })
 
 // SECTION 6
 // INFO
 sr.reveal('.quote', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 sr.reveal('.contact', {
-  duration: 1500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
   distance: '50%',
   origin: 'bottom'
 })
 // IMAGE
 sr.reveal('.map', {
-  duration: 1500,
-  delay: 1000,
+  duration: 2000,
+  delay: 800,
   opacity: 0
 })
 sr.reveal('.location', {
-  duration: 1500,
-  delay: 1250,
+  duration: 2000,
+  delay: 900,
   opacity: 0
 })
